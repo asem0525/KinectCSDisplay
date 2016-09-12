089426b8c62ca67b7d9296a0ac1194968f5fdd52
Ryan Wedoff <Ryan Wedoff>
2016-09-11 21:06:29 -0500

    Added weather to mainwindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="MainWindow.xaml.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
+﻿
 namespace Microsoft.Samples.Kinect.ControlsBasics
 {
     using System;
@@ -13,6 +8,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
     using Microsoft.Kinect.Wpf.Controls;
     using Microsoft.Samples.Kinect.ControlsBasics.DataModel;
     using System.Timers;
+    
     using System.Diagnostics;
     using System.Threading.Tasks;
     using System.Windows.Media.Animation;
@@ -21,6 +17,8 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
     using System.IO;
     using Newtonsoft.Json;
     using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using System.Windows.Media.Imaging;
 
 
     /// <summary>
@@ -31,10 +29,13 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
         private static Timer clockTimer;
         private static Timer bongoSwapTimer;
         private static Timer bongoGetTimer;
+        private static Timer weatherTimer;
         private static BongoData bongoData;
         private static int bongoIndex;
+        private static Simpleforecast weatherData;
         private static List<VisibleBongoData> fullBongoData = new List<VisibleBongoData>();
 
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class. 
         /// </summary>
@@ -57,7 +58,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             //Starts timer for clock
             SetClockTimer();
 
-            
+            //Set bongo timers
             SetBongoGetTimer();
             SetBongoData();
             SetBongoSwapTimer();
@@ -65,9 +66,23 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             //Sets the scrolling bottom bar text event
             StartBottomBar();
 
-            
+            //Sets timers and weather data
+            GetWeatherData();
+            SetWeatherTimer();
 
+        }
 
+        /// <summary>
+        /// Timer used to call an API update for Weather Data
+        /// </summary>
+        private void SetWeatherTimer()
+        {
+            // Create a timer with a 10 min interval.
+            weatherTimer = new Timer(600000);
+            // Hook up the Elapsed event for the timer. 
+            weatherTimer.Elapsed += GetWeatherData;
+            weatherTimer.AutoReset = true;
+            weatherTimer.Enabled = true;
         }
 
         /// <summary>
@@ -82,8 +97,102 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             Uri feedUri = new Uri("http://api.ebongo.org/prediction?format=json&stopid=0001&api_key=XXXX");
             using (WebClient downloader = new WebClient())
             {
-                downloader.DownloadStringCompleted += new DownloadStringCompletedEventHandler(downloader_DownloadStringCompleted);
+                downloader.DownloadStringCompleted += new DownloadStringCompletedEventHandler(downloader_DownloadStringCompletedBongo);
                 downloader.DownloadStringAsync(feedUri);
+            }
+        }
+
+
+        /// <summary>
+        /// Gets weather in JSON form from WeatherUnderground
+        /// Calls convsersion to WeatherData objects.
+        /// Only used on timer call
+        /// </summary>
+        private void GetWeatherData(object source, ElapsedEventArgs e)
+        {
+            //using (StreamReader r = new StreamReader("C:/Users/rwedoff/Desktop/tempWeather.json")) {
+            //    string json = r.ReadToEnd();
+                                
+            //    Rootobject root = JsonConvert.DeserializeObject<Rootobject>(json);
+            //    weatherData = root.forecast.simpleforecast;
+            //    SetWeatherData();
+            //}
+
+            //Weather Forcast from Weather Underground.com
+            Uri feedUri = new Uri("http://api.wunderground.com/api/75c131024c99cf58/forecast/q/IA/Iowa_City.json");
+            using (WebClient downloader = new WebClient())
+            {
+                downloader.DownloadStringCompleted += new DownloadStringCompletedEventHandler(downloader_DownloadStringCompletedWeather);
+                downloader.DownloadStringAsync(feedUri);
+            }
+        }
+
+        /// <summary>
+        /// Gets weather in JSON form from WeatherUnderground
+        /// Calls convsersion to WeatherData objects.
+        /// Only used on initalize
+        /// </summary>
+        private void GetWeatherData()
+        {
+            //Weather Forcast from Weather Underground.com
+            Uri feedUri = new Uri("http://api.wunderground.com/api/75c131024c99cf58/forecast/q/IA/Iowa_City.json");
+            using (WebClient downloader = new WebClient())
+            {
+                downloader.DownloadStringCompleted += new DownloadStringCompletedEventHandler(downloader_DownloadStringCompletedWeather);
+                downloader.DownloadStringAsync(feedUri);
+            }
+        }
+        /// <summary>
+        /// Gets the Bongo data (Json form) and creates BongoData objects
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void downloader_DownloadStringCompletedWeather(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                string responseStream = e.Result;
+                Rootobject root = JsonConvert.DeserializeObject<Rootobject>(responseStream);
+                weatherData = root.forecast.simpleforecast;
+                SetWeatherData();
+            }
+        }
+
+        /// <summary>
+        /// Sets the weather data with the icons on the mainwindow
+        /// </summary>
+        private void SetWeatherData()
+        {
+            if (weatherData != null)
+            {
+                int i = 0;
+                string hostIconURL = "http://icons.wxug.com/i/c/i/";
+                if(DateTime.Now.Hour >= 18)
+                {
+                    hostIconURL = hostIconURL + "nt_";
+                }
+                while (i <= 3 && i < weatherData.forecastday.Length)
+                {
+                    if (i == 0)
+                    {
+                        day1.Text = weatherData.forecastday[0].date.weekday;
+                        Temp1.Text = weatherData.forecastday[0].high.fahrenheit + "°/" + weatherData.forecastday[0].low.fahrenheit + "°";
+                        weatherIcon1.Source = new BitmapImage(new Uri(hostIconURL + weatherData.forecastday[0].icon + ".gif"));
+                    }
+                    else if(i == 1)
+                    {
+                        day2.Text = weatherData.forecastday[1].date.weekday;
+                        Temp2.Text = weatherData.forecastday[1].high.fahrenheit + "°/" + weatherData.forecastday[1].low.fahrenheit + "°";
+                        weatherIcon2.Source = new BitmapImage(new Uri(hostIconURL + weatherData.forecastday[1].icon + ".gif"));
+                    }
+                    else if (i == 2)
+                    {
+                        day3.Text = weatherData.forecastday[2].date.weekday;
+                        Temp3.Text = weatherData.forecastday[2].high.fahrenheit + "°/" + weatherData.forecastday[2].low.fahrenheit + "°";
+                        weatherIcon3.Source = new BitmapImage(new Uri(hostIconURL + weatherData.forecastday[2].icon + ".gif"));
+                    }
+                    i++;
+                }
             }
         }
 
@@ -92,12 +201,11 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void downloader_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        private void downloader_DownloadStringCompletedBongo(object sender, DownloadStringCompletedEventArgs e)
         {
             if (e.Error == null)
             {
                 string responseStream = e.Result;
-                //Debug.WriteLine(e.Result);
                 bongoData = JsonConvert.DeserializeObject<BongoData>(responseStream);
             }
             SetBongoData();
@@ -112,7 +220,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             // Create a timer with a two second interval.
             clockTimer = new Timer(1000);
             // Hook up the Elapsed event for the timer. 
-            clockTimer.Elapsed += setClockTimeEvent;
+            clockTimer.Elapsed += SetClockTimeEvent;
             clockTimer.AutoReset = true;
             clockTimer.Enabled = true;
         }
@@ -122,13 +230,17 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
         /// </summary>
         /// <param name="source"></param>
         /// <param name="e"></param>
-        private void setClockTimeEvent(Object source, ElapsedEventArgs e)
+        private void SetClockTimeEvent(Object source, ElapsedEventArgs e)
         {
-            Dispatcher.Invoke(() =>
+            try
             {
-                dateText.Text = DateTime.Now.Date.ToString("MMMM d, yyyy");
-                clockText.Text = DateTime.Now.ToString("h:mm:ss tt");
-            });
+                Dispatcher.Invoke(() =>
+                {
+                    dateText.Text = DateTime.Now.Date.ToString("MMMM d, yyyy");
+                    clockText.Text = DateTime.Now.ToString("h:mm:ss tt");
+                });
+            }
+            catch { }
         }
 
         /// <summary>
@@ -140,6 +252,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             bongoSwapTimer = new Timer(5000);
             // Hook up the Elapsed event for the timer. 
             bongoSwapTimer.Elapsed += SetBongoSwapEvent;
+            //bongoSwapTimer.Elapsed += SetSwapEvent2;
             bongoSwapTimer.AutoReset = true;
             bongoSwapTimer.Enabled = true;
         }
@@ -149,7 +262,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
         /// </summary>
         private void SetBongoGetTimer()
         {
-            // Create a timer with a two second interval.
+            // Create a timer with a 1 min interval.
             bongoGetTimer = new Timer(60000);
             // Hook up the Elapsed event for the timer. 
             bongoGetTimer.Elapsed += GetBusData;
@@ -168,6 +281,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
                 foreach (var bd in bongoData.predictions)
                 {
                     string minString = bd.minutes.ToString() + "min.";
+
                     if(bd.minutes == 0)
                     {
                         minString = "Arriving";
@@ -186,12 +300,16 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
                     {
                         colorString = "blue";
                     }
-                    fullBongoData.Add(new VisibleBongoData() { stopname = bd.stopname, minutes = minString, routename = bd.title, color = colorString });
+
+                    if(bd.minutes <= 15 || bongoData.predictions.Count <= 8)
+                    {
+                        fullBongoData.Add(new VisibleBongoData() { stopname = bd.stopname, minutes = minString, routename = bd.title, color = colorString });
+                    }
                 }
             }
             else
             {
-                fullBongoData.Add(new VisibleBongoData() { stopname = "No buses running" });
+                fullBongoData.Add(new VisibleBongoData() { stopname = "No buses running", color="#FFFFFF"});
             }
         }
 
@@ -206,7 +324,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             if(bongoIndex < fullBongoData.Count)
             {
                 int i = 0;
-                while (i + bongoIndex < fullBongoData.Count && i < 3)
+                while (i + bongoIndex < fullBongoData.Count && i < 4)
                 {
                     groupedBongoData.Add(fullBongoData[i + bongoIndex]);
                     i++;
@@ -217,12 +335,24 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             {
                 bongoIndex = 0;
             }
-            
-            Dispatcher.Invoke(() =>
+            try
             {
-                bongoList.ItemsSource = groupedBongoData;
-            });
+                //Udpdate UI thread with new bus group of 4
+                Dispatcher.Invoke(() =>
+                {
+                    if (groupedBongoData.Count >= 1)
+                    {
+                        bongoList.ItemsSource = groupedBongoData;
+                    }
+
+                });
+
+            }
+            catch { };
+
+
         }
+
 
         /// <summary>
         /// Handle a button click from the wrap panel.
